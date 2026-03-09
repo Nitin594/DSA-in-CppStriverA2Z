3f3e9eacafdcf03e20ce5b65f5ef30c38ef1c573
Nitin594 <nitinmaurya594@gmail.com>
2026-03-10 05:20:34 +0530

    Print n, n times using recurrsion

--- recurssion/Basics/printNtimes.cpp
+++ recurssion/Basics/printNtimes.cpp
@@ -1,29 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void printNtimes(int n)
+void printNtimes(int n, int count)
 {
-    int count = n;
-    if (n == 0)
+    if (count == 0)
         return;
-    cout << count;
-    // count--;
-    printNtimes(n-1);
+    cout << n;
+    count--;
+    printNtimes(n, count);
 }
 void printName(string name, int n)
 {
-    if(n == 0) return;
-    cout<<name<<" ";
-    printName(name, n-1);
+    if (n == 0)
+        return;
+    cout << name << " ";
+    printName(name, n - 1);
+}
+
+void print12N(int n)
+{
+    if (n == 0)
+        return;
+    print12N(n - 1);
+    cout << n << "\n";
+}
+void printN21(int n)
+{
+    if (n == 0)
+        return;
+    cout << n << "\n";
+    printN21(n - 1);
 }
 
 int main()
 {
     int n = 3;
-    printNtimes(n);
+    printNtimes(n, n);
 
     // print name n times
-    printName("Nitin",n);
+    // printName("Nitin",n);
+
+    // print12N(n);
+    // printN21(n);
 
     return 0;
 }
