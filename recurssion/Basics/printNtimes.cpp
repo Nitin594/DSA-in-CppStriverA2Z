6562bb4a2869b2660927cd548f564d45e96d4db0
Nitin594 <nitinmaurya594@gmail.com>
2026-03-10 06:14:12 +0530

    Basic recurrsion problems

--- recurssion/Basics/printNtimes.cpp
+++ recurssion/Basics/printNtimes.cpp
@@ -32,16 +32,39 @@ void printN21(int n)
     printN21(n - 1);
 }
 
+int sum = 0;
+int sumofN(int n)
+{
+    if (n == 0)
+        return sum;
+    sum = sum + n;
+    sumofN(n - 1);
+}
+
+int fact = 1;
+int factorial(int n)
+{
+    if (n < 0)
+        return 0;
+    if (n == 0 || n == 1)
+        return fact;
+    fact = n * (factorial(n - 1));
+}
+
 int main()
 {
-    int n = 3;
-    printNtimes(n, n);
+    int n = 5;
+    // printNtimes(n, n);
 
     // print name n times
     // printName("Nitin",n);
 
     // print12N(n);
     // printN21(n);
+
+    // cout << sumofN(n);
+
+    cout << factorial(n);
 
     return 0;
 }
